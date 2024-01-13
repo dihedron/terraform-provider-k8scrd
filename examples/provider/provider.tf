ed66207fbecd18ef1f191e298044cdd5d64a8ccf
Andrea Funtò <dihedron.dev@gmail.com>
2024-01-13 21:12:29 +0100

    Changed template to actual example CRD and objects

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -13,37 +13,73 @@ provider "k8scrd" {
   # example configuration here
 }
 
-
-resource "k8scrd_instance" "example1" {
+resource "k8scrd_instance" "crontab_crd" {
   attributes = {
-    token = "1234-5678-90ABCDEF-12345678-90AB-CDEF"
-    username = "myUsername1"
-    password = "myS3cr3tP4$$w0rd!!"
-    content = "Some other important stuff here for resource 1"
+    name = "crontabs.stable.example.com"
+    version = "v1"
+    served = "true"
+    storage = "true"
+    scope = "Namespaced"
   }
   template = <<-EOT
-  For running this template I'm going to use a security token (value: {{ .token }}")
-  and if the token has not been provided, I could use a username and password combination
-  for basic authentication (values: for the username it's {{ .username }} and for the 
-  password that would be {{ .password }}).
-  Last the content to store into the YAML is something like {{ .content }}.
-  EOT
+    apiVersion: apiextensions.k8s.io/v1
+    kind: CustomResourceDefinition
+    metadata:
+      # name must match the spec fields below, and be in the form: <plural>.<group>
+      name: {{ .name }}
+    spec:
+      # group name to use for REST API: /apis/<group>/<version>
+      group: stable.example.com
+      # list of versions supported by this CustomResourceDefinition
+      versions:
+        - name: {{ .version }}
+          # Each version can be enabled/disabled by Served flag.
+          served: {{ .served }}
+          # One and only one version must be marked as the storage version.
+          storage: {{ .storage }}
+          schema:
+            openAPIV3Schema:
+              type: object
+              properties:
+                spec:
+                  type: object
+                  properties:
+                    cronSpec:
+                      type: string
+                    image:
+                      type: string
+                    replicas:
+                      type: integer
+      # either Namespaced or Cluster
+      scope: {{ .scope }}
+      names:
+        # plural name to be used in the URL: /apis/<group>/<version>/<plural>
+        plural: crontabs
+        # singular name to be used as an alias on the CLI and for display
+        singular: crontab
+        # kind is normally the CamelCased singular type. Your resource manifests use this.
+        kind: CronTab
+        # shortNames allow shorter string to match your resource on the CLI
+        shortNames:
+        - ct  
+    EOT
 }
 
-
-resource "k8scrd_instance" "example2" {
+resource "k8scrd_instance" "crontab_object_1" {
   attributes = {
-    token = "1234-5678-90ABCDEF-12345678-90AB-CDEF"
-    username = "myUsername2"
-    password = "myS3cr3tP4$$w0rd!!"
-    content = "Some other important stuff here for resource 2"
+    kind = "crontabs.stable.example.com"
+    name = "my-new-cron-object-1"
+    cron = "* * * * */5"
+    image = "my-awesome-cron-image"
   }
   template = <<-EOT
-  For running this template I'm going to use a security token (value: {{ .token }}")
-  and if the token has not been provided, I could use a username and password combination
-  for basic authentication (values: for the username it's {{ .username }} and for the 
-  password that would be {{ .password }}).
-  Last the content to store into the YAML is something like {{ .content }}.
+    apiVersion: "{{- .kind -}}"
+    kind: CronTab
+    metadata:
+      name: {{ .name }}
+    spec:
+      cronSpec: "{{- .cron -}}"
+      image: {{ .image }}
   EOT
 }
 
